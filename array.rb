c6fa82d23840d7537e1980ca32125da3883953d9
maruleba <folaa1234@gmail.com>
2018-08-08 18:07:54 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,18 +1,7 @@
-# puts  "#{unny_books}
-# end
-ages = [16, 17, 17, 14]
-
-
-total = 0
-ages.each do |age|
-puts "Your age is  #{age}"
-total = total + age 
-end
-puts total
-end
-
 
 
 sports = ["basketball", 'football", "swimming", "hiking"']
+counter = 1
 sports.each do |sports|
-puts  "sports 
+puts  "sports #{counter}: #{sport}""
+coun
