60f7a800ffe21d3dd761e6b26d8c66aaf3acff70
maruleba <folaa1234@gmail.com>
2018-08-08 18:08:55 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,7 +1,11 @@
 
 
-sports = ["basketball", 'football", "swimming", "hiking"']
+sports = ["basketball","football", "swimming"]
+
+
 counter = 1
 sports.each do |sports|
-puts  "sports #{counter}: #{sport}""
-coun
+puts  "sports #{counter}: #{sport}"
+ counter = counter + 1 
+ end 
+ sports.each
